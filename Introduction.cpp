c8c8eb4471fb55ad0165c6edab68188531d06c5c
Shubhi Tiwari <shubhitiwari0132@gmail.com>
2026-05-28 20:23:57 +0530

    how to create and access arrays in singly linke lidt

---
+++ Introduction.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+public: 
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+int main(){
+    vector<int> arr = {2, 4, 6};
+    Node* y = new Node(arr[0], nullptr);
+
+    cout << y << endl;
+    cout << y->data << endl;
+
+    return 0;
+}
